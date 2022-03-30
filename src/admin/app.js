9ace7ac0a930c5846576b08aa9f5364ca9bb4fd5
Jithin K Satheesh <jithinksatheesh@gmail.com>
2022-03-31 00:46:24 +0530

    push

---
+++ src/admin/app.js
@@ -0,0 +1,13 @@
+// import MenuLogo from './assets/images/logo-strapi.png'
+
+export default {
+  config: {
+    menu : {
+      // logo: MenuLogo,
+    }
+    ,
+  },
+  bootstrap(app) {
+    console.log(app);
+  },
+};
